27246c26b91827f08d4c49211a750b9e1c727e19
romansechin <romansechin@gmail.com>
2024-09-07 11:39:50 +0300

    Добавьте файлы проекта.

---
+++ ParamsArgsProj/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("For counting average write numbers, other for quit:");
+        decimal[] numbers = { };
+        decimal temp;
+        while (true) {
+            if (decimal.TryParse(Console.ReadLine(), out temp))
+            {
+                numbers = [..numbers, temp];
+            }
+            else 
+            {
+                break;
+            }
+        }
+        Console.WriteLine($"Average = {Average(numbers)}");
+    }
+
+    public static double Average(params decimal[] numbers) { 
+        return (double) numbers.Average(x => x);
+    }
+}
